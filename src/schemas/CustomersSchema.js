9216e82ad70c20749ac4a68251570c95a2c99b96
RubsRafa <rubiarafaela2000@gmail.com>
2023-02-11 17:24:39 -0300

    test: cpf validation

--- src/schemas/CustomersSchema.js
+++ src/schemas/CustomersSchema.js
@@ -2,7 +2,7 @@ import joi from 'joi';
 
 export const customerSchema = joi.object({
         name: joi.string().not('').required(),
-        phone: joi.string().length(10,11).required(),
-        cpf: joi.string().length(11).numeric().required(),
+        phone: joi.string().min(10).max(11).required(),
+        cpf: joi.string().length(11).alphanum().required(),
         birthday: joi.date().iso("YYYY-MM-DD").required()
 })
