937fb82b06ec8b70846144914885cd9933a2df09
RubsRafa <rubiarafaela2000@gmail.com>
2023-02-11 17:33:35 -0300

    fix: insert client cpf

--- src/middlewares/postCustomerValidation.js
+++ src/middlewares/postCustomerValidation.js
@@ -7,6 +7,8 @@ export async function postCustomerValidation(req, res, next) {
 
         if(customer.name === "") return res.sendStatus(400);
 
+        if(typeof(Number(customer.cpf)) !== 'number') return res.sendStatus(400)
+
         const userCpfExist = await db.query('SELECT * FROM customers WHERE cpf = $1', [customer.cpf])
 
         if(userCpfExist.rows[0]) return res.sendStatus(409);
