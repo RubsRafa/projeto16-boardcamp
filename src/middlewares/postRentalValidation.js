f5cd074a59bf6fe95de9cbdb4798c6c9ed640194
RubsRafa <rubiarafaela2000@gmail.com>
2023-02-11 20:54:40 -0300

    fix: no game in stock

--- src/middlewares/postRentalValidation.js
+++ src/middlewares/postRentalValidation.js
@@ -13,7 +13,7 @@ export async function postRentalValidation (req, res, next) {
         
         if (rental.daysRented <= 0) return res.sendStatus(400)
 
-        if (gameIdExist.stockTotal <= 0) return res.sendStatus(400)
+        if (gameIdExist.rows[0].stockTotal <= 0) return res.sendStatus(400)
 
         res.locals.rental = rental;
         next(); 
