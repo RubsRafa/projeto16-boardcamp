ff47e1d1c1e3d7cc85a48433e2338e2c9d9ac840
RubsRafa <rubiarafaela2000@gmail.com>
2023-02-11 16:52:46 -0300

    fix: basic validation for price per day

--- src/schemas/GamesSchema.js
+++ src/schemas/GamesSchema.js
@@ -3,6 +3,6 @@ import joi from "joi";
 export const gameSchema = joi.object({
     name: joi.string().required(),
     image: joi.string().required(),
-    stockTotal: joi.number().positive().required(),
-    pricePerDay: joi.number().required()
+    stockTotal: joi.number().positive().not(0).required(),
+    pricePerDay: joi.number().positive().not(0).required()
 })
