4a93e74a97c36733b9b35265920270ec642f85ee
RubsRafa <rubiarafaela2000@gmail.com>
2023-02-11 10:42:19 -0300

    fix: get rentals using join

--- src/controllers/Rentals.js
+++ src/controllers/Rentals.js
@@ -3,8 +3,15 @@ import { db } from '../database/database.js';
 export async function getRentals(req, res) {
     try {
 
-        const rentals = await db.query('SELECT * FROM rentals;');
+        const rentals = await db.query(`
+        SELECT rentals.*, customers.* AS customer, games.* AS game 
+        FROM rentals 
+        JOIN customers 
+        ON customers.id = rentals."customerId"
+        JOIN games
+        ON games.id = rentals."gameId";`);
 
+        console.log(rentals.rows)
         return res.send(rentals.rows)
         
     } catch (error) {
